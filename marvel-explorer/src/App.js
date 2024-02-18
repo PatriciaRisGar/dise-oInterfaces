16aa8316219457729b173fe3245e18995c1b1aaa
PatriciaRisGar <xprisgar115@ieshnosmachado.org>
2024-02-18 17:46:09 +0100

    [diseñoInterfaces] - añado fondo trabajo react

--- marvel-explorer/src/App.js
+++ marvel-explorer/src/App.js
@@ -1,11 +1,21 @@
 import Personajes from './componentes/Personajes.jsx';
 import Container from '@mui/material/Container';
+import background from './media/fondo.jpg';
 
 function App() {
+	const containerStyle = {
+		backgroundImage: `url(${background}) `,
+		backgroundSize: '100% 100%', // Ajusta el tamaño de la imagen para cubrir todo el contenedor
+		backgroundRepeat: 'no-repeat',
+		height: '100vh',
+		width: '100%',
+	};
 	return (
-		<Container fixed>
-			<Personajes />
-		</Container>
+		<div style={containerStyle}>
+			<Container fixed>
+				<Personajes />
+			</Container>
+		</div>
 	);
 }
 
